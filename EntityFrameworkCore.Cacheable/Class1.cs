732e8f92175fe25179fbeaf38145d72e696035b7
Steffen Mangold <steffen.mangold@powerdoo.com>
2018-12-24 01:46:39 +0100

    Projektdateien hinzufügen.

---
+++ EntityFrameworkCore.Cacheable/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Cacheable
+{
+    public class Class12
+    {
+    }
+}
